ef848dbca78b718c472aafd11a259607ca149f49
TheRafal <rafal.mracz@gmail.com>
2023-02-13 21:20:15 +0100

    typingSpeed param is now a number

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,35 +1,39 @@
-import { Wrapper, Container, Options } from "../styles/Main.styled"
+import { Wrapper, Container, Options } from "../styles/Main.styled";
 import { useState, useEffect } from "react";
-import Typer from "../Typer"
+import Typer from "../Typer";
 import { useTheme } from "../../hooks/useTheme";
 
 function Main() {
-    const {theme} = useTheme();
-    const [showOptions, setShowOptions] = useState(false);
+  const { theme } = useTheme();
+  const [showOptions, setShowOptions] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setShowOptions(true);
-        }, 1700);
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowOptions(true);
+    }, 1700);
+  }, []);
 
-    return (
-        <div className="main">
-            <Wrapper>
-                <Container>
-                    <Typer text="welcome" typerStarter={`[${theme.colors.primary}]root[/]@[${theme.colors.secondary}]therafal.dev[/]:~$ `} typingSpeed="225" />
-                    {showOptions ? (
-                        <Options>
-                            <a href="/about-me">about-me</a>
-                            <a href="#">blog</a> 
-                        </Options>
-                    ) : (
-                        <Options />
-                    )}
-                </Container>
-            </Wrapper>
-        </div>
-    )
+  return (
+    <div className="main">
+      <Wrapper>
+        <Container>
+          <Typer
+            text="welcome"
+            typerStarter={`[${theme.colors.primary}]root[/]@[${theme.colors.secondary}]therafal.dev[/]:~$ `}
+            typingSpeed={225}
+          />
+          {showOptions ? (
+            <Options>
+              <a href="/about-me">about-me</a>
+              <a href="#">blog</a>
+            </Options>
+          ) : (
+            <Options />
+          )}
+        </Container>
+      </Wrapper>
+    </div>
+  );
 }
 
 export default Main;
